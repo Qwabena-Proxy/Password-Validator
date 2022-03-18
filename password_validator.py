56d286d234caca3e93e6fa89f528130c788cf0ea
Qwabena <100248770+Qwabena-Proxy@users.noreply.github.com>
2022-03-18 00:39:57 +0000

    Add files via upload

---
+++ password_validator.py
@@ -0,0 +1,16 @@
+special = ['!','@','#','$','%','&','*']
+num = ['1','2','3','4','5','6','7','8','9','0']
+fc = 0
+sc = 0
+password = input('Enter:')
+ln = len(password)
+for i in password:
+  if i in special:
+    fc += 1
+  if i in num:
+    sc += 1
+if fc >= 2 and sc >= 2 and ln == 14:
+  print('Strong')
+else:
+  print('Weak')
+print(num.index('0'))
